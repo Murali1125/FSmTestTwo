edd661e856fb4eab09a904128ec244b2d75730c6
Murali Krishna <murali1125s@gmail.com>
2022-06-12 15:47:05 +0530

    commit4

--- Handlers.js
+++ Handlers.js
@@ -1,13 +1,15 @@
 function UpdateUser(){
     let userName=""
     let userNameEle = document.getElementById("userName");
-    if(userName ){
-        userName = userNameEle.value
+    if(userNameEle ){
+        userName = userNameEle.value;
+        console.log(userName)
     }
     let userAge= ""
-    let userAgeEle = document.getElementById("userAge")?.value;
-    if(userAgeEle ){
+    let userAgeEle = document.getElementById("userAge");
+    if(userNameEle ){
         userName = userAgeEle.value
+        console.log(userName)
     }
     window.alert(`Welcome ${userName}, ${userAge}`)
 }
