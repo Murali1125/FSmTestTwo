c4590a124c92b16cf78327e9f6889b6216bf62f9
Murali Krishna <murali1125s@gmail.com>
2022-06-12 15:36:58 +0530

    initial commit

---
+++ Handlers.js
@@ -0,0 +1,5 @@
+function UpdateUser(){
+    let userName = document.getElementById("userName");
+    let userAge = document.getElementById("userAge");
+    window.alert(`Welcome ${userName}, ${userAge}`)
+}
